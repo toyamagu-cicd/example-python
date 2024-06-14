87a5173efd2cf8e4b0a6985f80526676adb36bf3
toyamagu2021 <toyamagu2021@gmail.com>
2024-06-14 17:43:37 +0900

    test
    Signed-off-by: toyamagu2021 <toyamagu2021@gmail.com>

--- app/calculator.py
+++ app/calculator.py
@@ -18,6 +18,10 @@ class Calculator:
         return x * y
 
     def divide(x, y):
+        if x > 1000:
+            print('x is greater than 0')
+        if x < -1000:
+            print('x is greater than 0')
         if y == 0:
             return 'Cannot divide by 0'
         return x * 1.0 / y
